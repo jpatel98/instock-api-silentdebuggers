138b9870690121751a112a1b251b4ebf11168834
Jigar Patel <jigar@JP-machine.local>
2022-07-18 17:29:01 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const inventoryRoutes = require('./routes/inventory');
+const warehouseRoutes = require('./routes/warehouse')
+const cors = require('cors');
+
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/inventory', inventoryRoutes);
+app.use('/warehouse', warehouseRoutes);
+
+app.listen(8080, () => {
+    console.log(`Server fired up on port 8080 🫡`);
+});
