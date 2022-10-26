57725c889fa08e84917ee66c0123401af0c591b4
Jigar Patel <jigar@JP-machine.local>
2022-10-26 15:22:57 -0400

    add migration file

--- migrations/20221022011126_create_warehouse_inventory_tables.js
+++ migrations/20221022011126_create_warehouse_inventory_tables.js
@@ -1,9 +1,31 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('warehouse', (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw("(UUID())"));
+        table.string('name').notNullable();
+        table.string('position').notNullable().defaultTo('Store Manager');
+        table.string('manager').notNullable();
+        table.string('address').notNullable();
+        table.string('phone').notNullable();
+        table.string('email').notNullable();
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('inventory', (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw("(UUID())"));
+        table.string('name').notNullable();
+        table.string('description').notNullable();
+        table
+          .integer('warehouse_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('warehouse')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+        table.integer('quantity').notNullable().defaultTo(0);
+        table.string('status').notNullable();
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+      });
 };
 
 /**
@@ -11,5 +33,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('inventory').dropTable('warehouse');
 };
