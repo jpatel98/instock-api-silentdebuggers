3a4d7717a5674272a28fbd046bf9ab6c614313d5
Jigar Patel <jigar@JP-machine.local>
2022-10-26 19:29:21 -0400

    Add endpoint to get single warehouse details based on the id

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const warehouseController = require('../controllers/warehouseController');
 
 router.route('/').get(warehouseController.index);
+router.route('/:id').get(warehouseController.singleWarehouse)
 
 module.exports = router;
