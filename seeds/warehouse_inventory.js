39c0c689babc2a5338f5fa77a9220827d9c3ed55
Jigar Patel <jigar@JP-machine.local>
2022-10-26 18:06:05 -0400

    seed inital data and add knex scripts

--- seeds/warehouse_inventory.js
+++ seeds/warehouse_inventory.js
@@ -1,13 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+const inventoryData = require('../seed_data/inventory');
+const warehouseData = require('../seed_data/warehouse');
+
+exports.seed = function (knex) {
+  return knex('warehouse')
+    .del()
+    .then(function () {
+      return knex('warehouse').insert(warehouseData);
+    })
+    .then(() => {
+      return knex('inventory').del();
+    })
+    .then(() => {
+      return knex('inventory').insert(inventoryData);
+    });
 };
