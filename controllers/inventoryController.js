5e94ca3ab5ae3966b01652801344665ee86f94c2
Jigar Patel <jigar@JP-machine.local>
2022-10-27 21:35:32 -0400

    POST new inventory item endpoint

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,18 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+
+exports.addInventory = (req, res) => {
+  // Validate the request body for required data
+  if (!req.body.itemName || !req.body.description || !req.body.warehouseID || !req.body.warehouseName || !req.body.quantity || !req.body.status || !req.body.category) {
+    return res.status(400).send('Please make sure to provide itemName, description, warehouseID, warehouseName, quantity, status and category fields in a request');
+  }
+  knex('inventory')
+    .insert(req.body)
+    .then((data) => {
+      // For POST requests we need to respond with 201 and the location of the newly created record
+      const newInventoryURL = `/inventory/${data[0]}`;
+      res.status(201).location(newInventoryURL).send(newInventoryURL);
+    })
+    .catch((err) => res.status(400).send(`Error creating Inventory Item: ${err}`));
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventoryController');
 
-router.route('/').get(inventoryController.index);
+router
+    .route('/')
+    .get(inventoryController.index)
+    .post(inventoryController.addInventory)
 
 module.exports = router;
